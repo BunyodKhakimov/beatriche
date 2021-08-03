c33dd26035e368759187c873747fe14dc7533432
BunyodKhakimov <b.khakimov@student.inha.uz>
2021-08-03 18:39:40 +0500

    not authenticated user order & review create

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -15,7 +15,8 @@ class OrderRequest extends FormRequest
     public function authorize()
     {
         // only allow updates if the user is logged in
-        return backpack_auth()->check();
+        // return backpack_auth()->check();
+        return true;
     }
 
     /**
